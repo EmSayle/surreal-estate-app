d3b3478604c3d11365663c533c5744f3f4baf2c5
Emily Sayle <emsayle@gmail.com>
2019-04-02 20:29:12 +0100

    added css for addProperties page

--- src/components/addProperties.jsx
+++ src/components/addProperties.jsx
@@ -36,52 +36,39 @@ class AddProperties extends Component {
     return (
       <div>
         <h1 className="addProps">Add Properties</h1>
-        <main>
+
           <form onSubmit={this.handleAddProperty}>
-            <label>Title
-              <input name="title" value={this.state.fields.title} onChange={this.handleFieldChange} />
-            </label>
-            <p />
-            <label>Property Type
-              <select name="type" value={this.state.fields.type} onChange={this.handleFieldChange}>
-                <option value="Studio Apartment">Studio Apartment</option>
-                <option value="Flat">Flat</option>
-                <option value="Detached">Detached</option>
-                <option value="Semi-Detached">Semi-Detached</option>
-                <option value="End of Terrace">End of Terrace</option>
-                <option value="Cottage">Cottage</option>
-              </select>
-            </label>
-            <p />
-            <label>Bedrooms
-              <input name="bedrooms" value={this.state.fields.bedrooms} onChange={this.handleFieldChange} />
-            </label>
-            <p />
-            <label>Bathrooms
-              <input name="bathrooms" value={this.state.fields.bathrooms} onChange={this.handleFieldChange} />
-            </label>
-            <p />
-            <label>City
-              <select name="city" value={this.state.fields.city} onChange={this.handleFieldChange}>
-                <option value="Manchester">Manchester</option>
-                <option value="Liverpool">Liverpool</option>
-                <option value="Leeds">Leeds</option>
-                <option value="Sheffield">Sheffield</option>
-                <option value="Chester">Chester</option>
-              </select>
-            </label>
-            <p />
-            <label>Price
-              <input name="price" value={this.state.fields.price} onChange={this.handleFieldChange} />
-            </label>
-            <p />
-            <label>Email
-              <input name="email" value={this.state.fields.email} onChange={this.handleFieldChange} />
-            </label>
-            <p />
-            <button>Add</button>
+
+            <input placeholder="Title" name="title" value={this.state.fields.title} onChange={this.handleFieldChange} />
+
+            <select className="select" name="type" value={this.state.fields.type} onChange={this.handleFieldChange}>
+              <option value="Studio Apartment">Studio Apartment</option>
+              <option value="Flat">Flat</option>
+              <option value="Detached">Detached</option>
+              <option value="Semi-Detached">Semi-Detached</option>
+              <option value="End of Terrace">End of Terrace</option>
+              <option value="Cottage">Cottage</option>
+            </select>
+
+            <input placeholder="Number of Bedrooms" name="bedrooms" value={this.state.fields.bedrooms} onChange={this.handleFieldChange} />
+
+            <input placeholder="Number of Bathrooms" name="bathrooms" value={this.state.fields.bathrooms} onChange={this.handleFieldChange} />
+
+            <select className="select" name="city" value={this.state.fields.city} onChange={this.handleFieldChange}>
+              <option value="Manchester">Manchester</option>
+              <option value="Liverpool">Liverpool</option>
+              <option value="Leeds">Leeds</option>
+              <option value="Sheffield">Sheffield</option>
+              <option value="Chester">Chester</option>
+            </select>
+
+            <input type="" placeholder="Enter Price"name="price" value={this.state.fields.price} onChange={this.handleFieldChange} />
+
+            <input placeholder="Email Address" name="email" value={this.state.fields.email} onChange={this.handleFieldChange} />
+
+            <button className="button2">Add</button>
           </form>
-        </main>
+
       </div>
     );
   }
